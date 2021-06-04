29da68f2b0854f1590cf4f024817c732ad59955c
Marek Schmidt <info@msdev.me>
2021-06-04 20:55:29 +0200

    As you can see you can see something

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -1,68 +1,56 @@
 import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
-import { css, cx } from 'emotion';
-import { stylesFactory, useTheme } from '@grafana/ui';
+import { css } from 'emotion';
+// import { stylesFactory, useTheme } from '@grafana/ui';
+import * as Leaf from 'leaflet';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
-export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
-  const theme = useTheme();
-  const styles = getStyles();
-  return (
-    <div
-      className={cx(
-        styles.wrapper,
-        css`
-          width: ${width}px;
-          height: ${height}px;
-        `
-      )}
-    >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: `${theme.isLight ? theme.palette.greenBase : theme.palette.blue95}` }} r={100} />
-        </g>
-      </svg>
+export class SimplePanel extends React.Component<Props> {
+  private mapId: string;
 
-      <div className={styles.textBox}>
-        {options.showSeriesCount && (
-          <div
-            className={css`
-              font-size: ${theme.typography.size[options.seriesCountSize]};
-            `}
-          >
-            Number of series: {data.series.length}
-          </div>
-        )}
-        <div>Text option value: {options.text}</div>
-      </div>
-    </div>
-  );
-};
+  constructor(props: Readonly<Props> | Props) {
+    super(props);
 
-const getStyles = stylesFactory(() => {
-  return {
-    wrapper: css`
-      position: relative;
-    `,
-    svg: css`
-      position: absolute;
-      top: 0;
-      left: 0;
-    `,
-    textBox: css`
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      padding: 10px;
-    `,
-  };
-});
+    const id = props.id;
+    this.mapId = `special-map-${id}`;
+  }
+
+  componentDidMount() {
+    const map = Leaf.map(this.mapId, {
+      worldCopyJump: true,
+      preferCanvas: true,
+      zoom: 1,
+      center: { lat: 49, lng: 8 },
+    });
+
+    const tileDetails = {
+      url: 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: 'abc',
+    };
+
+    Leaf.tileLayer(tileDetails.url, {
+      maxZoom: 18,
+      subdomains: tileDetails.subdomains,
+      detectRetina: true,
+      attribution: tileDetails.attribution,
+    }).addTo(map);
+  }
+
+  render() {
+    const id = this.props.id;
+    this.mapId = `special-map-${id}`;
+
+    return (
+      <div
+        id={this.mapId}
+        className={css`
+          widht: 100%;
+          height: 100%;
+        `}
+      ></div>
+    );
+  }
+}
